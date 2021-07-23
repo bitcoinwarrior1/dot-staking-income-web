cc6ede6821db5dc39d00f435f942d99ed127e8cc
James Sangalli <j.l.sangalli@gmail.com>
2021-07-24 08:13:47 +1000

    working with server

--- index.js
+++ index.js
@@ -2,33 +2,43 @@ const request = require("superagent");
 const { Parser } = require('json2csv');
 const download = require("downloadjs");
 const parser = new Parser();
-const url = "https://dot-staking-income.herokuapp.com/";
+const url = "https://dot-staking-income.herokuapp.com/history";
 
 document.addEventListener("DOMContentLoaded", () => {
 
     document.getElementById("downloadButton").addEventListener("click", () => {
         const query = getQuery();
+        document.getElementById("status").hidden = false;
         request.get(query.url, (err, result) => {
-            if(err) throw err;
-            let stakingHistory = result.data.history;
-            stakingHistory.push({ total_value_usd: result.data.total_value_usd });
-            const csv = parser.parse(stakingHistory);
-            download(csv, `staking-income-${query.network}.csv`, "text/plain");
+            if(err) {
+                document.getElementById("status").innerText = err;
+                throw err;
+            } else {
+                try {
+                    let stakingHistory = result.body.list;
+                    stakingHistory.push({ total_value_usd: result.body.total_value_usd });
+                    const csv = parser.parse(stakingHistory);
+                    download(csv, `staking-income-${query.network}.csv`, "text/plain");
+                    document.getElementById("status").hidden = true;
+                } catch (e) {
+                    document.getElementById("status").innerText = "no staking found on this address";
+                }
+            }
         });
     });
 
     function getQuery() {
         const userAddress = document.getElementById("address").value;
-        const isKSM = document.getElementById("DOT").checked;
+        const isKSM = document.getElementById("KSM").checked;
         if(isKSM) {
            return {
                network: "KSM",
-               url: `${url}/${userAddress}/kusama`
+               url: `${url}/${userAddress}/KSM`
            }
         } else {
             return {
                 network: "DOT",
-                url: `${url}/${userAddress}/polkadot`
+                url: `${url}/${userAddress}/DOT`
             }
         }
     }
