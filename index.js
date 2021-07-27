4ec3643e0fd2029a888a47da46b116e9d726113d
James Sangalli <j.l.sangalli@gmail.com>
2021-07-27 17:19:40 +1000

    add user address to csv file naming #4

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     let stakingHistory = result.body.list;
                     stakingHistory.push({ total_value_usd: result.body.total_value_usd });
                     const csv = parser.parse(stakingHistory);
-                    download(csv, `staking-income-${query.network}.csv`, "text/plain");
+                    download(csv, `staking-income-${query.network}-${query.address}.csv`, "text/plain");
                     document.getElementById("status").hidden = true;
                 } catch (e) {
                     document.getElementById("status").innerText = "no staking found on this address";
@@ -33,11 +33,13 @@ document.addEventListener("DOMContentLoaded", () => {
         if(isKSM) {
            return {
                network: "KSM",
+               address: userAddress,
                url: `${url}/${userAddress}/KSM`
            }
         } else {
             return {
                 network: "DOT",
+                address: userAddress,
                 url: `${url}/${userAddress}/DOT`
             }
         }
